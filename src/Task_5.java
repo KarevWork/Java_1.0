e81a458beba2b31963f4f3c30be0f23c744776a3
KarevWork <karev.work@gmail.com>
2020-01-12 23:35:00 +0300

    Task #5

---
+++ src/Task_5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Task_5 {
+    public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int mt= scan.nextInt();
+        int q,w,e,r,t,y,u,i,o,p;
+                q=mt;
+        System.out.println("Your number * 1: "+q);
+                w=mt*2;
+        System.out.println("Your number * 2: "+w);
+                e=mt*3;
+        System.out.println("Your number * 3: "+e);
+                r=mt*4;
+        System.out.println("Your number * 4: "+r);
+                t=mt*5;
+        System.out.println("Your number * 5: "+t);
+                y=mt*6;
+        System.out.println("Your number * 6: "+y);
+                u=mt*7;
+        System.out.println("Your number * 7: "+u);
+                i=mt*8;
+        System.out.println("Your number * 8: "+i);
+                o=mt*9;
+        System.out.println("Your number * 9: "+o);
+                p=mt*10;
+        System.out.println("Your number * 10: "+p);
+    }
+}
