e8a28c83f487dd746b1dbc7afec00d4cd4e9ca01
KarevWork <karev.work@gmail.com>
2020-01-19 21:40:40 +0300

    Task #8

---
+++ src/Task_8.java
@@ -0,0 +1,11 @@
+public class Task_8 {
+    public static void main(String[] args) {
+        int x=1;
+        while(x<=50) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
+
+
