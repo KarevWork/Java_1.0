3b455b7bc9712788afb0cbb34ab1a38186a613f2
KarevWork <karev.work@gmail.com>
2020-01-19 21:47:09 +0300

    Task #8 add another solution

--- src/Task_8.java
+++ src/Task_8.java
@@ -1,10 +1,15 @@
 public class Task_8 {
     public static void main(String[] args) {
-        int x=1;
+        // 1 вариант
+        /*int x=1;
         while(x<=50) {
             System.out.println(x);
             x++;
-        }
+        }*/
+       // 2 вариант
+       for (int i=1; i<=50; i++){
+           System.out.println(i);
+       }
     }
 }
 
