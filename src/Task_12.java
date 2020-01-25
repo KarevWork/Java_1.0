ec150ad3266effdec656c536a687d8d417453927
KarevWork <karev.work@gmail.com>
2020-01-26 00:37:04 +0300

    Task #12

---
+++ src/Task_12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task_12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите любой набор символов, добавляя пробелы: ");
+        String userString = scan.nextLine();
+        System.out.print("Ваши символы без пробелов: "+ userString.replace(" ", ""));
+    }
+}
