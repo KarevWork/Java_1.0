9bdc8d4c576321f6efe9e7451748ec76bbaaa21b
KarevWork <karev.work@gmail.com>
2020-02-09 19:50:38 +0300

    FinalTask #1 edit

--- src/FinalTask_1.java
+++ src/FinalTask_1.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class FinalTask_1 {
     public static void main(String[] args) {
+        int decInput, dec_value, base, temp, last_digit;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите число в десятичном формате: ");
-        int a, b;
-        a = scan.nextInt();
-        String temp = "";
-        while (a != 0) {
-            b = a % 2;
-            temp = b + temp;
-            a = a / 2;
+        System.out.print("Введите число в бинарном формате: ");
+        decInput = scan.nextInt();
+        dec_value = 0;
+        base = 1;
+        while (decInput > 0) {
+            last_digit = decInput % 10;
+            decInput = decInput / 10;
+            dec_value += last_digit * base;
+            base = base * 2;
         }
-        System.out.println(temp);
+        System.out.println("Ваше число в десятичном формате: " + dec_value);
     }
 }
